10b7f5008ae56dc471742b22e362923b4b8f2a26
Arish Kumar <97384467+Jimzical@users.noreply.github.com>
2023-04-10 11:13:17 +0530

    Update client.py
    better

--- client.py
+++ client.py
@@ -1,46 +1,77 @@
 import os
 import socket
-from tqdm import tqdm
+import re
+import json
 
-ip = "192.168.56.56"
-# ip = socket.gethostbyname(socket.gethostname()) #To get IP address of the local host
-port = 4455
-addr = (ip, port)
-size = 1024
-format = "utf-8"
+def InitializeClient(ip,port = 4455,filename = "file.txt",size = 1024,format = "utf-8"):
+    '''
+    This function is used to create a client socket and connect to the server
+    '''
+    addr = (ip,port)
+    filesize = os.path.getsize(filename)
+    client_data = {"ip":ip,"port":port,"filename":filename,"filesize":filesize,"size":size,"format":format,"addr":addr}
+    return client_data
 
-filename = "file.txt"
-filesize = os.path.getsize(filename)
 
+def ip_verify(ip):
+    '''
+    Using Regular Expression to verify the IP Address
+    '''
+    # if nicknames.json doesnt exist make one
+    if not os.path.exists("nicknames.json"):
+        with open("nicknames.json","w") as f:
+            json.dump({},f)
+
+    # read nicknames.json
+    with open("nicknames.json","r") as f:
+        nicknames = json.load(f)
+    # check if ip is in nicknames.json
+    if ip in nicknames:
+        print("Welcome Back {} ({})".format(ip,nicknames[ip]))
+        ip = nicknames[ip]
+        return ip
+    else:
+        nickname = input("Enter your nickname: ")
+        pattern = re.compile(r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+        if pattern.match(ip):
+            print("IP Address Verified")
+            nicknames[nickname] = ip
+            with open("nicknames.json","w") as f:
+                json.dump(nicknames,f)
+                print("Welcome {}".format(nickname))
+            return ip
+        else:
+            print("Invalid IP Address")
+            print("Transfer Failed")
+            return False
 
 
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    #AF_INET is used to designate the type of IP address the socket communicates in (Here it's IPv4)
-    #SOCK_STREAM specifies that is a TCP socket.
+    # ip = "192.168.235.1"
+    ip = input("Enter Server Ip Address or Nickname:")
+    ip = ip_verify(ip)
 
-    client.connect(addr) #To connect to the server 
+    if ip:        
+        client_data = InitializeClient(ip)
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        print(client_data["addr"])
+        client.connect(client_data["addr"]) 
 
-    data = f"{filename}_{filesize}"
-    client.send(data.encode(format)) #encodes the data in utf-8 and sends it to the server
+        data = "{}_{}".format(client_data["filename"],client_data["filesize"])
+        client.send(data.encode(client_data["format"])) #encodes the data in utf-8 and sends it to the server
 
-    msg = client.recv(size).decode(format) #To receive acknowledgement msg from server
-    print(f"SERVER: {msg}") #Decodes and prints the message
+        msg = client.recv(client_data["size"]).decode(client_data["format"]) #To receive acknowledgement msg from server
+        print(f"SERVER: {msg}") #Decodes and prints the message
 
-   # progress = tqdm(range(filesize), f"Sending {filename}", unit = "B", unit_scale = True, unit_divisor = filesize )
 
-    f = open(filename, "r") 
+    f = open(client_data["filename"], "r") 
 
     while True:
-        data = f.read(filesize)
-
+        data = f.read(client_data["size"])
         if not data:
             break
-
-        client.send(data.encode(format))
-        msg = client.recv(filesize).decode(format)
-
-        #progress.update(len(data))
+        client.send(data.encode(client_data["format"]))
+        msg = client.recv(client_data["size"]).decode(client_data["format"])
     
     client.close()
     f.close()
