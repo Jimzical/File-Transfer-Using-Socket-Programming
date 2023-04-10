e23247f8d6b32ecac83566f512ad47896e451aea
Arish Kumar <97384467+Jimzical@users.noreply.github.com>
2023-04-10 11:13:56 +0530

    Update server.py
    BETTER

--- server.py
+++ server.py
@@ -1,35 +1,38 @@
-import os
 import socket
-from tqdm import tqdm
 
-ip = socket.gethostbyname(socket.gethostname()) #To get IP address of the local host
-port = 4455
-addr = (ip, port)
-size = 1024
-format = "utf-8"
+def InitializeServer():
+    ip = socket.gethostbyname(socket.gethostname()) #To get IP address of the local host
+    port = 4455
+    addr = (ip, port)
+    size = 1024
+    format = "utf-8"
+
+    server_data = {"ip":ip,"port":port,"addr":addr,"size":size,"format":format}
+    return server_data
 
 
 def main():
+    server_data = InitializeServer()
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     #AF_INET is used to designate the type of IP address the socket communicates in (Here it's IPv4)
     #SOCK_STREAM specifies that is a TCP socket. 
 
-    server.bind(addr) #Assign the IP address and port number to the socket instance we created
+    server.bind(server_data["addr"]) #Assign the IP address and port number to the socket instance we created
     server.listen()
 
-    print("Sever is listening...")
+    print("Server is listening...")
 
     conn, address = server.accept()
     print(f"Client connected from {address[0]}:{address[1]} ")
 
-    data  = conn.recv(size).decode(format)
+    data  = conn.recv(server_data["size"]).decode(server_data["format"])
     print(data)
 
     item = data.split("_") #To retrieve file name and file size from the data received
     filename = item[0]
-    filesize = item[1]
 
-    conn.send("Filename and file size received".encode(format))
+    conn.send("Filename and file size received".encode(server_data["format"]))
 
     #progress = tqdm(range(filesize), f"Receiving {filename}", unit = "B", unit_scale = True, unit_divisor = filesize )
 
@@ -37,13 +40,13 @@ def main():
     f = open(f"recv_{filename}","w")
 
     while True:
-        data = conn.recv(size).decode(format)
+        data = conn.recv(server_data["size"]).decode(server_data["format"])
 
         if not data:
             break
 
         f.write(data)
-        conn.send("data received".encode(format))
+        conn.send("data received".encode(server_data["format"]))
 
         #progress.update(len(data))
     
